273d3d03740d2ed35626a91bc0600730c75de411
callumrowston <callum.rowsto@gmail.com>
2022-11-05 12:44:46 +1100

    Added all canyon instances to db seed

--- src/controllers/cli_controller.py
+++ src/controllers/cli_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from init import db, bcrypt
+from datetime import date, datetime
 from models.user import User
 from models.canyon import Canyon
 
@@ -25,7 +26,12 @@ def seed_db():
             is_admin=True
         ),
         User(
-            name='John Smith',
+            name='Callum_Rowston',
+            email='callum_r@canyon.com',
+            password=bcrypt.generate_password_hash('callum123').decode('utf-8')
+        ),
+        User(
+            name='John_Smith',
             email='johnsmith@canyon.com',
             password=bcrypt.generate_password_hash('12345').decode('utf-8')
         )
@@ -41,43 +47,77 @@ def seed_db():
             longest_abseil='25m',
             difficulty='Medium',
             wetsuits_recommended=False,
-            last_updated=
+            last_updated=datetime.now()
         ),
         Canyon(
-            name=,
-            area=,
-            description=,
-            estimated_time_hrs=,
-            number_abseils=,
-            longest_abseil=,
-            difficulty=,
-            wetsuits_recommended=,
-            last_updated=
+            name='Firefly',
+            area='Newnes',
+            description='A good canyon to the north of the WOlgan Valley with many short abseils and swims.',
+            estimated_time_hrs=8,
+            number_abseils=7,
+            longest_abseil='20m',
+            difficulty='Medium',
+            wetsuits_recommended=True,
+            last_updated=datetime.now()
         ),
         Canyon(
-            name=,
-            area=,
-            description=,
-            estimated_time_hrs=,
-            number_abseils=,
-            longest_abseil=,
-            difficulty=,
-            wetsuits_recommended=,
-            last_updated=
+            name='Rocky Creek',
+            area='South Wolgan',
+            description='A long, dark and spectacular canyon in the South Wolgan. A great beginner canyon as there is no abseiling required and be easily linked up with Twister Canyon to make up a full day',
+            estimated_time_hrs=4,
+            number_abseils=0,
+            longest_abseil='N/A',
+            difficulty='Easy',
+            wetsuits_recommended=True,
+            last_updated=datetime.now()
         ),
         Canyon(
-            name=,
-            area=,
-            description=,
-            estimated_time_hrs=,
-            number_abseils=,
-            longest_abseil=,
-            difficulty=,
-            wetsuits_recommended=,
-            last_updated=
+            name='Twister',
+            area='South Wolgan',
+            description='A short canyon that runs off Rocky Creek canyon. Features many jumps and slides and no abseils, making it ideal for beginners',
+            estimated_time_hrs=3,
+            number_abseils=0,
+            longest_abseil='N/A',
+            difficulty='Easy',
+            wetsuits_recommended=True,
+            last_updated=datetime.now()
+        ),
+        Canyon(
+            name='Tiger Snake',
+            area='South Wolgan',
+            description='A narrow twisting canyon with an upper and lower constriction. A usually dry canyon that makes it ideal to do in winter',
+            estimated_time_hrs=7,
+            number_abseils=5,
+            longest_abseil='25m',
+            difficulty='Easy-Medium',
+            wetsuits_recommended=False,
+            last_updated=datetime.now()
+        ),
+        Canyon(
+            name='Whungee Wheengee',
+            area='Wollangambe',
+            description='An excellent, sustained but difficult canyon with tight constrictions, many short abseils, swims, jumps and possible duck unders in high water.',
+            estimated_time_hrs=10,
+            number_abseils=6,
+            longest_abseil='15m',
+            difficulty='Medium-Hard',
+            wetsuits_recommended=True,
+            last_updated=datetime.now()
+        ),
+        Canyon(
+            name='Claustral',
+            area='Carmarthen Labyrinth',
+            description='Likely the most impressive canyon in NSW. Three back-to-back abseils lead to a long, sustained moss-covered canyon section lasting up to 1km. A long, difficult day with lots of bouldering, abseiling, swims and a veyr long hike out',
+            estimated_time_hrs=10,
+            number_abseils=5,
+            longest_abseil='20m',
+            difficulty='Medium',
+            wetsuits_recommended=True,
+            last_updated=datetime.now()
         )
     ]
 
     print("Tables seeded")
     db.session.add_all(users)
+    db.session.add_all(canyons)
     db.session.commit()
