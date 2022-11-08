59d3ba6b1ba920f49da13c508dcbb1220548daa0
callumrowston <callum.rowsto@gmail.com>
2022-11-09 10:55:37 +1100

    CheckAdded user login route and admin authorization

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, abort
 from init import db, bcrypt
+from datetime import date, timedelta
 from models.user import User, UserSchema
 from flask_jwt_extended import create_access_token, get_jwt_identity
 from sqlalchemy.exc import IntegrityError
@@ -32,11 +33,24 @@ def register_user():
 
 @auth_bp.route('/login/', methods=['POST'])
 def login_user():
-    pass
+
+    # Query DB for matching email 
+    stmt = db.select(User).filter_by(email=request.json['email'])
+    user = db.session.scalar(stmt)
+
+    # If user exists and password is correct then create token for user with 24hr time limit
+    if user and bcrypt.check_password_hash(user.password, request.json['password']):
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
+        return {'email': user.email, 'token': token, 'is_admin': user.is_admin}
+    else:
+        return {'error': 'Invalid email or password'}, 401
 
 def authorize_user():
-    # user_id = get_jwt_identity()
-    # stmt = db.select(User).filter_by(id=user_id)
-    # user = db.session.scalar(stmt)
-    # if not user.is_admin:
-    #     abort(401)
+    #Check if a user has admin privileges
+    user_id = get_jwt_identity()
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt)
+
+    if not user.is_admin:
+        abort(401, description='Access denied. You are not an administrator')
+        
