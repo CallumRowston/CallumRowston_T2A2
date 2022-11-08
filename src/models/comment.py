373b9bebdb1f570d8194a8ebffc3df9328e4c913
callumrowston <callum.rowsto@gmail.com>
2022-11-09 10:16:48 +1100

    Added FKs and relationships to comment model

--- src/models/comment.py
+++ src/models/comment.py
@@ -8,12 +8,14 @@ class Comment(db.Model):
     message = db.Column(db.String(250))
     date_posted = db.Column(db.Date)
 
-    # canyon_id = db.Column(db.Integer, db.ForeignKey('canyons.id'), nullable=False)
-    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    # canyon = db.relationship('Canyon', back_populates='comments')
-    # user = db.relationship('User', back_populates='comments')
+    canyon_id = db.Column(db.Integer, db.ForeignKey('canyons.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    canyon = db.relationship('Canyon', back_populates='comments')
+    user = db.relationship('User', back_populates='comments')
     
 class CommentSchema(ma.Schema):
+
+    # Validation
     # user = fields.Nested('UserSchema', only=['name', 'email'])
     # canyon = fields.Nested('CanyonSchema')
 
