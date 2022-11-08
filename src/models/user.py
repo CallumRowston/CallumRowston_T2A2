9ccaaac31418d6acc115a1cfe57c827a95e38e7f
callumrowston <callum.rowsto@gmail.com>
2022-11-09 10:22:11 +1100

    Added relationships to user model

--- src/models/user.py
+++ src/models/user.py
@@ -9,8 +9,8 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
-    # canyons = db.relationship('Canyon', back_populates='user', cascade='all, delete')
-    # comments = db.relationship('Comment', back_populates='user', cascade='all, delete')
+    canyons = db.relationship('Canyon', back_populates='user', cascade='all, delete')
+    comments = db.relationship('Comment', back_populates='user', cascade='all, delete')
 
 class UserSchema(ma.Schema):
 
