cee6847162b3178059bfe491cf42ac6985cd69f1
callumrowston <callum.rowsto@gmail.com>
2022-11-09 13:06:37 +1100

    Added create comment route and updated create canyon route

--- src/controllers/canyon_controller.py
+++ src/controllers/canyon_controller.py
@@ -2,6 +2,10 @@ from flask import Blueprint, request
 from init import db
 from datetime import date
 from models.canyon import Canyon, CanyonSchema
+from models.user import User, UserSchema
+from models.comment import Comment, CommentSchema
+from auth_controller import authorize_user
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 canyons_bp = Blueprint('canyons', __name__, url_prefix='/canyons')
 
@@ -20,34 +24,41 @@ def get_one_canyon(id):
     return {'error': f'Canyon not found with id {id}'}, 404
 
 @canyons_bp.route('/', methods=['POST'])
-# @jwt_required()
+@jwt_required()
 def create_canyon():
+    authorize_user()
     data = CanyonSchema().load(request.json)
 
-    canyon = Canyon(
-        name = data['name'],
-        area = data['area'],
-        description = data['description'],
-        estimated_time_hrs = data['estimated_time_hrs'],
-        number_abseils = data['number_abseils'],
-        longest_abseil = data['longest_abseil'],
-        difficulty = data['difficulty'],
-        wetsuits_recommended = data['wetsuits_recommended'],
-        last_updated = date.today(),
-        # user_id = get_jwt_identity()
-    )
+    stmt = db.select(User).filter_by(id=get_jwt_identity)
+    user = db.session.scalar(stmt)
 
-    db.session.add(canyon)
-    db.session.commit()
-    return {
-            'message': f'You have added {canyon.name} Canyon successfully',
-            'canyon': CanyonSchema().dump(canyon)
-    }
-    # return CanyonSchema().dump(canyon), 201
+    if user:
+        canyon = Canyon(
+            name = data['name'],
+            area = data['area'],
+            description = data['description'],
+            estimated_time_hrs = data['estimated_time_hrs'],
+            number_abseils = data['number_abseils'],
+            longest_abseil = data['longest_abseil'],
+            difficulty = data['difficulty'],
+            wetsuits_recommended = data['wetsuits_recommended'],
+            last_updated = date.today(),
+            user_id = get_jwt_identity()
+        )
+
+        db.session.add(canyon)
+        db.session.commit()
+        return {
+                'message': f'You have added {canyon.name} Canyon successfully',
+                'canyon': CanyonSchema().dump(canyon)
+        }
+    else:
+        return {'error': f'You must be logged in to add a canyon'}, 404
 
 @canyons_bp.route('/<int:id>/', methods=['PUT', 'PATCH'])
-# @jwt_required()
+@jwt_required()
 def update_one_canyon(id):
+    authorize_user()
     stmt = db.select(Canyon).filter_by(id=id)
     canyon = db.session.scalar(stmt)
     if canyon:
@@ -65,9 +76,9 @@ def update_one_canyon(id):
     return {'error': f'Canyon not found with id: {id}'}, 404
 
 @canyons_bp.route('/<int:id>/', methods=['DELETE'])
-# @jwt_required()
+@jwt_required()
 def delete_one_canyon(id):
-    # authorize()
+    authorize_user()
     stmt = db.select(Canyon).filter_by(id=id)
     canyon = db.session.scalar(stmt)
     if canyon:
@@ -75,3 +86,24 @@ def delete_one_canyon(id):
         db.session.commit()
         return {'message': f'{canyon.name} Canyon with id: {canyon.id} deleted.'}
     return {'error': f'Canyon not found with id: {id}'}, 404
+
+# -------------------------
+# ~~~~~~~ COMMENTS ~~~~~~~~
+# -------------------------
+
+@canyons_bp.route('/<int:id>/comments', methods=['POST'])
+@jwt_required()
+def create_comment(id):
+    stmt = db.select(Canyon).filter_by(id=id)
+    canyon = db.session.scalar(stmt)
+    if canyon:
+        comment = Comment(
+                message = request.json['message'],
+                date_posted = date.today(),
+                canyon_id = id,
+                user_id = get_jwt_identity()
+        )
+        db.session.add(comment)
+        db.session.commit()
+        return CommentSchema().dump(comment)
+    return {'error': f'canyon not found with id {id}'}, 404
